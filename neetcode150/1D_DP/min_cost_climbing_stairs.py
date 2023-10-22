059f46d4c66fe859a0ce48ab8a6ce1ab8cab507f
Harsh Shelar <harshshelar22@gmail.com>
2023-10-22 19:50:27 -0400

    comments and corrections to the solution

--- neetcode150/1D_DP/min_cost_climbing_stairs.py
+++ neetcode150/1D_DP/min_cost_climbing_stairs.py
@@ -1,13 +1,18 @@
 class Solution:
     def min_cost_climbing_stairs(self, cost):
         n = len(cost)
+
+        # we create an array where dp[i] is equal to the min cost needed to reach that step
+
         dp = [0] * n
         dp[0] = cost[0]
         dp[1] = cost[1]
 
         for i in range(1, n):
+            # min cost will be min of current cost + 1 step back and current cost + 2 step back
             dp[i] = min(cost[i] + dp[i - 1], cost[i] + dp[i - 2])
 
-        return dp[ - 1]
+        # we take min here because we can jump from n - 1 step as well without cost
+        return min(dp[n - 1], dp[n - 2])
 
 
