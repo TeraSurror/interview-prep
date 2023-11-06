4e798fcc819affde76094d3c88d52c2f445c596a
Harsh Shelar <harshshelar22@gmail.com>
2023-11-06 12:16:47 -0500

    solution for level order ancestor

---
+++ neetcode150/trees/level_order_ancestor.py
@@ -0,0 +1,22 @@
+import collections
+
+
+class Solution:
+    def levelOrder(self, root):
+        res = []
+        q = collections.deque()
+        if root:
+            q.append(root)
+
+        while q:
+            val = []
+
+            for i in range(len(q)):
+                node = q.popleft()
+                val.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(val)
+        return res
