73c36ee827e0d839bfd2f83e6d2725c0c1ec9486
Harsh Shelar <harshshelar22@gmail.com>
2024-02-07 17:53:35 -0500

    intervals: insert interval

---
+++ neetcode150/intervals/insert_intervals.py
@@ -0,0 +1,23 @@
+from hmac import new
+from typing import List
+
+
+def intervals(intervals: List[List[int]], newInterval):
+
+    result = []
+
+    for i in range(len(intervals)):
+        if newInterval[1] < intervals[i][0]:
+            result.append(newInterval)
+            return result + intervals[i:]
+        elif newInterval[0] > intervals[i][1]:
+            result.append(intervals[i])
+        else:
+            newInterval = [
+                min(newInterval[0], intervals[i][0]),
+                max(newInterval[1], intervals[i][1]),
+            ]
+
+    result.append(newInterval)
+
+    return result
