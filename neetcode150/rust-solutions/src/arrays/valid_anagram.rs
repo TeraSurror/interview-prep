e91003b6fb4deee6ad485ad74af74678d65f1983
Harsh Shelar <harshshelar22@gmail.com>
2024-02-25 19:18:39 -0500

    Arrays: Valid Anagram solution in Rust

---
+++ neetcode150/rust-solutions/src/arrays/valid_anagram.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+fn valid_anagrams(s: String, t: String) -> bool {
+    if s.len() != t.len() {
+        return false;
+    }
+
+    let mut map: HashMap<char, i32> = HashMap::new();
+
+    for (s_letter, t_letter) in s.chars().zip(t.chars()) {
+        *map.entry(s_letter).or_default() += 1; // or_default inserts the default value and returns a mutable ref.
+        *map.entry(t_letter).or_default() -= 1;
+    }
+
+    map.into_values().all(|cnt| cnt == 0)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn true_test() {
+        let s1 = String::from("anagram");
+        let s2 = String::from("nagaram");
+        assert!(valid_anagrams(s1, s2));
+    }
+
+    #[test]
+    fn false_test() {
+        let s1 = String::from("anagram");
+        let s2 = String::from("sadffan");
+        assert!(!valid_anagrams(s1, s2));
+    }
+
+    #[test]
+    fn blank_test() {
+        let s1 = String::from("anagram");
+        let s2 = String::from("");
+        assert!(!valid_anagrams(s1, s2));
+    }
+}
